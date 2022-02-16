2093cfe1dca50b6ad8cbfee112e26f780a3afde5
Nezumi-sama <kryska007@yandex.ru>
2022-02-17 00:50:20 +0300

    Second proj05

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,10 +1,8 @@
 package ru.netology.sqr;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SQRServiceTest {
